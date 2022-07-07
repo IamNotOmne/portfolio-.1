4b1ae9c8227b494e71fdf5ed5360d550354b65f3
IamNotOmne <21103810@usc.edu.ph>
2022-07-07 21:06:28 +0800

    my portfolio

---
+++ Omne Portfolio Website/assets/js/main.js
@@ -0,0 +1,73 @@
+/*=============== SHOW SIDEBAR ===============*/
+
+/*===== SIDEBAR SHOW =====*/
+/* Validate If Constant Exists */
+
+/*===== SIDEBAR HIDDEN =====*/
+/* Validate If Constant Exists */
+
+/*=============== SKILLS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContent = document.querySelectorAll('[data-content]')
+
+      tabs.forEach(tab => {
+        tab.addEventListener("click", () => {
+            const target = document.querySelector(tab.dataset.target)
+
+            tabContent.forEach(tabContents => {
+                tabContents.classList.remove('skills__active')
+            })
+
+            target.classList.add('skills__active')
+
+            tabs.forEach( tab => {
+                tab.classList.remove('skills_active')
+            })
+
+            tab.classList.add('skills__active')
+        })
+      })
+
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+let mixerPortfolio = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+/*===== Link Active Work =====*/
+const linkWork = document.querySelectorAll('.work__item')
+
+function activeWork() {
+    linkWork.forEach(l=> l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+
+linkWork.forEach(l => l.addEventListener("click", activeWork))
+
+/*===== Work Popup =====*/
+document.addEventListener("click", (e)=>{
+    if(e.target.classList.contains("work__button"))
+        togglePortfolioPopup();
+        portfolioItemDetails(e.target.parentElement);
+})
+
+function togglePortfolioPopup() {
+    document.querySelector(".portfolio__popup").classList.toggle("open");
+}
+
+document.querySelector("portfolio__popup-close").addEventListener("click", togglePortfolioPopup)
+
+/*=============== SERVICES MODAL ===============*/
+
+/*=============== SWIPER TESTIMONIAL ===============*/
+
+/*=============== INPUT ANIMATION ===============*/
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+/*=============== SHOW SCROLL UP ===============*/
